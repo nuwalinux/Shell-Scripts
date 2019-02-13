748f5d40de026efa40db711ffc46a89cbb6cbcd1
Nuwan Kaushalya <gforcenuwankaushalya@gmail.com>
2019-02-14 04:38:48 +0530

    Add files via upload
    notify when define file was updated

---
+++ updater.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#notify when file updated
+#script by Nuwan Kaushalya
+LTIME=`stat -c %Z /testscript/test`
+Aline=`wc -l < /testscript/test`
+
+while true
+do
+   ATIME=`stat -c %Z /testscript/test`
+
+   if [[ "$ATIME" != "$LTIME" ]]
+   then
+       Bline=`wc -l < /testscript/test`
+       let count=$Bline-$Aline
+       echo "$count" > /tmp/count
+       msg=`cat /tmp/count`
+       zenity --info --title "hiii nuwan" --text "updted $msg line" --display=:0
+       Aline=`wc -l < /testscript/test`
+       LTIME=$ATIME
+   fi
+       sleep 05
+done
